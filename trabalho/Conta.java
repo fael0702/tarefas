73468beb76366eb6ed0f2d2c9ff299b733eb86ac
fael0702 <rafacilio00@gmail.com>
2022-05-17 23:52:12 +0000

    tarefa de java

---
+++ trabalho/Conta.java
@@ -0,0 +1,21 @@
+package trabalho;
+
+public class Conta {
+
+    protected double saldo;
+
+    void deposita(double valor) {
+        this.saldo += valor;
+    }
+    void saca(double valor) {
+        this.saldo -= valor;
+    }
+    double getSaldo() {
+        return this.saldo;
+    }
+
+    void atualiza(double taxa) {
+        this.saldo += this.saldo * taxa;
+    }
+
+}
---
+++ trabalho/ContaCorrente.java
@@ -0,0 +1,11 @@
+package trabalho;
+
+public class ContaCorrente extends Conta{
+
+    void atualiza(double taxa) {
+        this.saldo += this.saldo * taxa * 2;
+    }
+    void deposita(double valor) {
+        this.saldo += valor - 0.10;
+    }
+}
